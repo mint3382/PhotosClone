a6d513557c0967ec37e109b9f082ccc77a469080
mint3382 <mskim0926@naver.com>
2024-08-08 18:15:15 +0900

    ✨(feat) PhotoCell 구현

---
+++ PhotosClone/PhotosClone/PhotoCell.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoCell.swift
+//  PhotosClone
+//
+//  Created by minsong kim on 8/8/24.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell, Identifiable {
+    static let id = "PhotoCell"
+    var representedAssetIdentifier: String = ""
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    func configureImage(image: UIImage?) {
+        imageView.image = image
+        configureImageUI()
+    }
+    
+    private func configureImageUI() {
+        self.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+}
