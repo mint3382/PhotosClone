a2db56f957e0fd598c9d7fb596f74436f72c349f
mint3382 <mskim0926@naver.com>
2024-08-08 23:21:07 +0900

    ✨(feat) UIControl extension 구현

---
+++ PhotosClone/PhotosClone/DesignSystem/UIControl+.swift
@@ -0,0 +1,59 @@
+//
+//  UIControl+.swift
+//  PhotosClone
+//
+//  Created by minsong kim on 8/8/24.
+//
+
+import Combine
+import UIKit
+
+extension UIControl {
+    struct EventPublisher: Publisher {
+        typealias Output = Void
+        typealias Failure = Never
+
+        fileprivate var control: UIControl
+        fileprivate var event: Event
+
+        func receive<S: Subscriber>(subscriber: S) where S.Input == Output, S.Failure == Failure {
+            let subscription = EventSubscription<S>()
+            subscription.target = subscriber
+
+            subscriber.receive(subscription: subscription)
+
+            control.addTarget(
+                subscription,
+                action: #selector(subscription.trigger),
+                for: event
+            )
+        }
+    }
+}
+
+private extension UIControl {
+    class EventSubscription<Target: Subscriber>: Subscription where Target.Input == Void {
+
+        var target: Target?
+
+        func request( _ demand: Subscribers.Demand) { }
+
+        func cancel() {
+            target = nil
+        }
+
+        @objc
+        func trigger() {
+            _ = target?.receive(())
+        }
+    }
+}
+
+extension UIControl {
+    func publisher(for event: Event) -> EventPublisher {
+        EventPublisher(
+            control: self,
+            event: event
+        )
+    }
+}
