da005455aa97bc6dee361aab49c885b314569559
mint3382 <mskim0926@naver.com>
2024-08-09 20:04:26 +0900

    ✨(feat) HeaderView 구현

---
+++ PhotosClone/PhotosClone/DesignSystem/HeaderView.swift
@@ -0,0 +1,43 @@
+//
+//  HeaderView.swift
+//  PhotosClone
+//
+//  Created by minsong kim on 8/9/24.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+    static let identifier = "HeaderView"
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(titleLabel)
+        configureLabelUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureLabel(text: String) {
+        titleLabel.text = text
+    }
+
+    func configureLabelUI() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+}
